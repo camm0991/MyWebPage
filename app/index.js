81b1e47f91b153d3536486e121fceda49181d08e
camm0991 <carlos.mtz0991@gmail.com>
2016-01-04 00:36:08 -0600

    Articles and side news now are readed from json

--- app/index.js
+++ app/index.js
@@ -3,112 +3,126 @@
  */
 (function myBlog() {
 
-//Attributes and init
+    //Attributes and init
     var app = {
         DOMapi: domApiFunc(),
         dataApi: dataApiFunc(),
-        //addArticles: addArticlesFunc,
+        addArticles: addArticlesFunc,
         addSideNews: addSideNewsFunc,
         mainContainer: null,
-        sections : null,
+        sections: null,
         menu: null,
         init: init
     };
 
     function init() {
         this.addSideNews();
+        this.addArticles();
     };
-//END Attributes and init
+    //END Attributes and init
 
-//Side news
+    //Side news
     function addSideNewsFunc() {
-        
+
         function generateSideNews() {
             var newsContainer = this.DOMapi.getContainer('#newsContainer');
             var newsList = document.createElement('nav');
             newsContainer.appendChild(newsList);
 
             function addList(item) {
-                newsList.innerHTML += "<li><a href=''>" + item.title 
-                + "</a></li>";
+                newsList.innerHTML += "<li><a href=''>" + item.title + "</a></li>";
             }
             this.DOMapi.addItems(this.menu, addList);
         }
 
         function addMenuToDOM(obj) {
-            console.log(obj.data.sections);
             this.sections = obj.data.sections;
             this.menu = obj.data.menu;
             generateSideNews.call(this);
         }
 
-        this.dataApi.getData(addMenuToDOM.bind(this))
+        this.dataApi.getData(addMenuToDOM.bind(this));
     };
-//END Side news
+    //END Side news
 
-//Articles
+    //Articles
     //Creates new section blocks with retrieved news
-    /*function addArticlesFunc(obj) {
-        this.mainContainer = this.DOMapi.getSectionContainer("#sections");
+    function addArticlesFunc() {
 
-        //Define section elements
-        var section = document.createElement('section');
-        var article = document.createElement('article');
-        var div_header = document.createElement('div');
-        var header = document.createElement('header');
-        var header_content = document.createElement('h2');
-        var header_button = document.createElement('button');
-        var div_content = document.createElement('div');
-        var image = document.createElement('img');
-        var content = document.createElement('p');
+        function generateArticle() {
+            var sectionContainer = this.DOMapi.getContainer("#sections");
 
-        //Create section structure
-        section.appendChild(article)
+            function addArticle(item) {
+                //Define section elements
+                var section = document.createElement('section');
+                var article = document.createElement('article');
+                var div_header = document.createElement('div');
+                var header = document.createElement('header');
+                var header_content = document.createElement('h2');
+                var header_button = document.createElement('button');
+                var div_content = document.createElement('div');
+                var image = document.createElement('img');
+                var content = document.createElement('p');
 
-        //Header
-        article.appendChild(div_header);
-        div_header.appendChild(header);
-        header.appendChild(header_content);
-        header.appendChild(header_button);
-        header_content.innerHTML = 'Generic title';
-        header_button.innerHTML = 'Generic button';
+                //Create section structure
+                section.appendChild(article)
 
-        //Content
-        article.appendChild(div_content);
-        div_content.className = 'article-content';
-        //Add image
-        div_content.appendChild(image);
-        image.src = 'images/generic.png';
-        image.style.width = '200px';
-        image.style.height = '200px';
-        //Add content
-        div_content.appendChild(content);
-        content.className = 'font-monospace';
-        content.innerHTML = 'Generic information';
+                //Header
+                article.appendChild(div_header);
+                div_header.appendChild(header);
+                header.appendChild(header_content);
+                header.appendChild(header_button);
+                header_button.innerHTML = 'Generic button';
+                header_content.innerHTML = item.title;
 
-        //Add new section to the sections 
-        this.mainContainer[0].appendChild(section)
-    }*/
+                //Content
+                article.appendChild(div_content);
+                div_content.className = 'article-content';
+                //Add image
+                div_content.appendChild(image);
+                image.style.width = '200px';
+                image.style.height = '200px';
+                image.src = item.image;
+                //Add content
+                div_content.appendChild(content);
+                content.className = 'font-monospace';
+                content.innerHTML = item.article;
 
-    function domApiFunc(){
-        function getContainer(id){
+
+                //Add new section to the sections 
+                sectionContainer.appendChild(section);
+            }
+            this.DOMapi.addItems(this.sections, addArticle);
+        }
+
+        function addArticleToDom(obj) {
+            this.sections = obj.data.sections;
+            this.menu = obj.data.menu;
+            generateArticle.call(this);
+        }
+
+        this.dataApi.getData(addArticleToDom.bind(this));
+    }
+
+    function domApiFunc() {
+        function getContainer(id) {
             return document.querySelector(id);
         }
 
-        function addItems(items, callback){
-            for (var i=0; i< items.length; i++){
+        function addItems(items, callback) {
+            for (var i = 0; i < items.length; i++) {
                 callback(items[i]);
             };
         }
-        var publicAPI ={
-            getContainer : getContainer,
+        var publicAPI = {
+            getContainer: getContainer,
             addItems: addItems
         }
         return publicAPI;
     };
-//END Articles
+    //END Articles
 
-//Data
+    //Data
     //Reads JSON file
     function dataApiFunc() {
 
@@ -131,10 +145,10 @@
             getData: getData
         }
     }
-//END Data
+    //END Data
 
-//Execution
+    //Execution
     //Execute update
     app.init();
-//END Execution
+    //END Execution
 })();
